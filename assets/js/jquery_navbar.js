1039acf1884d4feaf2fc73627e363a84a29a8ebc
Stephen Nicholas <stephennicholas.191200@gmail.com>
2021-01-02 00:32:35 +0700

    Added Dropdown menu

--- assets/js/jquery_navbar.js
+++ assets/js/jquery_navbar.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $(document).scroll(function () {
         var $nav = $("nav");
+        var $dropdown_item = $(".dropdown-content");
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        $dropdown_item.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 })
