cc4b1c91bde26bdf07dd4cc88d2f1e0024d7957a
Stephen Nicholas <stephennicholas.191200@gmail.com>
2021-01-03 12:26:04 +0700

    Changes on logo website and user interface

--- assets/js/jquery_navbar.js
+++ assets/js/jquery_navbar.js
@@ -2,7 +2,12 @@ $(document).ready(function(){
     $(document).scroll(function () {
         var $nav = $("nav");
         var $dropdown_item = $(".dropdown-content");
+        var $logo = $(".logo");
+        var $rightnav = $("#right-nav");
+
+        $logo.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
         $dropdown_item.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        $rightnav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 })
